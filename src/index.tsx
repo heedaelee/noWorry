3c01f86d084ba55cb0f6d3f02422a4ffb5c2114b
heedaelee <adguy72@gmail.com>
2023-11-16 14:18:13 +0900

    feat: 🐲 루트에 RecoilRoot 설치

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import {BrowserRouter} from 'react-router-dom';
+import {RecoilRoot} from 'recoil';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 // PWA 활성화
 serviceWorkerRegistration.register();
