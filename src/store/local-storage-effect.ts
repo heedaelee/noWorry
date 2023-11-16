fb53a6828b711199db6bda0756f958b1242574d4
heedaelee <adguy72@gmail.com>
2023-11-16 14:20:48 +0900

    add: 로컬스토리지 effect 생성 및 추가

---
+++ src/store/local-storage-effect.ts
@@ -0,0 +1,14 @@
+export const localStorageEffect =
+  <T>(key: string) =>
+  ({setSelf, onSet}: any) => {
+    const savedValue = localStorage.getItem(key);
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue));
+    }
+
+    onSet((newValue: T, _: any, isReset: boolean) => {
+      isReset
+        ? localStorage.removeItem(key)
+        : localStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
