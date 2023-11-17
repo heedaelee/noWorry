2335033036c083baf1f700ceb5dd8cf21bda8480
heedaelee <adguy72@gmail.com>
2023-11-17 16:52:03 +0900

    add : 공통 스타일 코드 추가
    styles/common.ts

---
+++ src/styles/common.ts
@@ -0,0 +1,44 @@
+export const Colors = {
+  black: '#000',
+  white: '#FFF',
+  green: '#42AA9D',
+  blue: '#2663D6',
+};
+
+// export const ColorBase = {
+//   Primary: '#7547F2',
+//   PrimaryBorder: '#B195FF',
+//   Secondary: '#F4F0FF',
+// };
+
+export const ColorText = {
+  Default: '#000',
+  SubText: '#808080',
+  NotSelectedBottomIcons: '#979797',
+  Disabled: '#717680',
+};
+
+// export const ColorContents = {
+//   Line: '#DEDEDE',
+//   messageLine: '#6327FF',
+//   tooltipBg: '#393D40',
+// };
+
+export const Fonts = {
+  Default: 'Noto Sans KR',
+};
+
+export const backgroundColor = '#F5F5F5';
+
+export const BoxShadow = {
+  shadowColor: '#000',
+  shadowOffset: {
+    width: 0,
+    height: 3,
+  },
+  shadowOpacity: 0.27,
+  shadowRadius: 10,
+  elevation: 6,
+  backgroundColor: backgroundColor,
+  borderRadius: 10,
+};
