6aef9cc9fa2c275ba07778d66d3b3491835c9a5d
heedaelee <adguy72@gmail.com>
2023-11-17 18:53:35 +0900

    refactoring : GlobalStyles.ts 파일 리팩토링
    css 형식, font 부분을 globalHtmlStyles.ts로 옮기고, 컬러, 폰트를 js 형식으로 정의함

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,40 +1,44 @@
-import {createGlobalStyle} from 'styled-components';
+const GlobalStyles = {
+  Colors: {
+    black: '#000',
+    white: '#FFF',
+    green: '#42AA9D',
+    blue: '#2663D6',
+  },
+  ColorText: {
+    Default: '#000',
+    SubText: '#808080',
+    NotSelectedBottomIcons: '#979797',
+    Disabled: '#717680',
+  },
+  Fonts: {
+    Default: 'Noto Sans KR',
+  },
+  backgroundColor: '#F5F5F5',
+  BoxShadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 10,
+    elevation: 6,
+    backgroundColor: '#F5F5F5',
+    borderRadius: 10,
+  },
+};
 
-const GlobalStyles = createGlobalStyle`
-/*아래 폰트*/
-@font-face {
-  font-family: "NotoSansKR-Bold";
-  src: url("src/assets/fonts/NotoSansKR-Bold.ttf");
-}
+//  const ColorBase = {
+//   Primary: '#7547F2',
+//   PrimaryBorder: '#B195FF',
+//   Secondary: '#F4F0FF',
+// };
 
-@font-face {
-  font-family: "NotoSansKR-Medium";
-  src: url("src/assets/fonts/NotoSansKR-Medium.ttf");
-}
-
-@font-face {
-  font-family: "NotoSansKR-Regular" ;
-  src: url("src/assets/fonts/NotoSansKR-Regular.ttf");
-}
-
-body {
-  margin: 0;
-  padding: 0;
-  font-family: NotoSansKR-Medium sans-serif;
-  /* box-sizing: border-box; */
-}
-
-* {
-  box-sizing: inherit;
-}
-
-input, button, textarea {
-  font-family: inherit;
-}
-
-html, body, #root {
-  height: 100%;
-}
-`;
+//  const ColorContents = {
+//   Line: '#DEDEDE',
+//   messageLine: '#6327FF',
+//   tooltipBg: '#393D40',
+// };
 
 export default GlobalStyles;
