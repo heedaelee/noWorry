b439f0d526dda7d50da0d7803c0dd187e84d1b4e
heedaelee <adguy72@gmail.com>
2023-11-18 18:53:48 +0900

    add : RoundFilterButton 생성
    issue : isSelected 같은 custom 속성을 DOM에 추가할땐 only 소문자로 넣든지 앞에 커스텀 표시 "$" 를 붙여야 한다고 한다.

---
+++ src/components/buttons/RoundFilterButton.tsx
@@ -0,0 +1,42 @@
+import Text from 'components/text/Text';
+import {useState} from 'react';
+import styled from 'styled-components';
+import GlobalStyles from 'styles/globalStyles';
+
+interface RoundFilterButtonProps {
+  buttonText: string;
+  $isSelected: boolean;
+  onClick: (text: string) => void;
+  style?: React.CSSProperties;
+}
+
+const RoundFilterButton = ({
+  buttonText,
+  $isSelected,
+  onClick,
+}: RoundFilterButtonProps) => {
+  console.log($isSelected);
+  return (
+    <RoundFilterButtonStyled
+      $isSelected={$isSelected}
+      onClick={() => onClick(buttonText)}>
+      <Text type='button' color={$isSelected ? 'white' : 'black'}>
+        {buttonText}
+      </Text>
+    </RoundFilterButtonStyled>
+  );
+};
+
+const RoundFilterButtonStyled = styled.button<
+  Pick<RoundFilterButtonProps, '$isSelected'>
+>`
+  background-color: ${({$isSelected}) =>
+    $isSelected ? GlobalStyles.Colors.green : 'none'};
+  align-items: center;
+  padding: 8px 10px;
+  border-radius: 16px;
+  border: ${({$isSelected}) =>
+    $isSelected ? 'none' : `1px solid ${GlobalStyles.Colors.green}`};
+`;
+
+export default RoundFilterButton;
