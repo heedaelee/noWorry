09f4e5bb7c5dabc429aa78b4d3c760b3e4ec65b8
heedaelee <adguy72@gmail.com>
2023-11-21 19:06:21 +0900

    refactoring : 라우터 통계 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import {Route, Routes, useNavigate} from 'react-router-dom';
 
 import Container from 'components/common/Container';
 import Main from 'pages/Main';
+import Statics from 'pages/Statics';
 
 function App() {
   return (
     <Routes>
       <Route path='/' Component={Container}>
         <Route path='/' Component={Main} />
+        <Route path='/statics' Component={Statics} />
       </Route>
     </Routes>
   );
