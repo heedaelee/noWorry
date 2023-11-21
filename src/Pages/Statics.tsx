80f09065201d2a26ba8e9918a5ae9f7da5e01098
heedaelee <adguy72@gmail.com>
2023-11-21 19:06:04 +0900

    add : 통계 컴포넌트 추가

---
+++ src/Pages/Statics.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import {PaddingWrapper} from 'styles/globalStyles';
+
+const Statics = () => {
+  return <PaddingWrapper>테스트</PaddingWrapper>;
+};
+
+export default Statics;
