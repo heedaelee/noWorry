eb357569cd414649f23833ceecc46e9c7d8daf01
heedaelee <adguy72@gmail.com>
2023-11-23 00:07:15 +0900

    refactoring : Header 변경

--- src/components/headers/Headers.tsx
+++ src/components/headers/Headers.tsx
@@ -1,24 +1,54 @@
-import Text from 'components/text/Text';
-import {RxGear} from 'react-icons/rx';
 import styled from 'styled-components';
-import {headWrapper} from 'styles/globalStyles';
+import {IoIosArrowBack} from 'react-icons/io';
+
+import Text from 'components/text/Text';
+import {headerConstant} from 'constants/hedaer-constant';
+import {useChangePages} from 'hooks/useChagePages';
+import {
+  HorizontalPaddingWrapper,
+  headWrapper,
+  mainPadding,
+} from 'styles/globalStyles';
 
 const Headers = () => {
+  const [pages, setPages] = useChangePages();
+
+  const {page: pageConstants} = headerConstant;
+
+  /* page랑 page상수값이랑 일치할때, title,Icon 가져옴 */
+  const {title, ...Others} = pageConstants[pages];
+  const isActiveBackButton = ['register', 'editor', 'setting'].includes(pages);
   return (
-    <HeadWrapper>
-      <Text type='h1'>나의 걱정들</Text>
-      <RxGear size={28} />
+    <HeadWrapper $isActiveBackButton={isActiveBackButton}>
+      {isActiveBackButton && (
+        <BackButton>
+          <IoIosArrowBack size='20px' color='#00201C' />
+        </BackButton>
+      )}
+      <Text type='h1'>{title}</Text>
+      {Others.IconComponent && <Others.IconComponent />}
     </HeadWrapper>
   );
 };
 
 export default Headers;
 
-const HeadWrapper = styled.div`
+const HeadWrapper = styled.div<{$isActiveBackButton: boolean}>`
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
-  text-align: center;
+  justify-content: ${({$isActiveBackButton}) =>
+    $isActiveBackButton ? 'center' : 'space-between'};
+  align-items: center;
   height: ${headWrapper}px;
+  padding: ${mainPadding}px ${mainPadding}px 0px ${mainPadding}px;
   /* border: 1px solid black; */
+`;
+
+const BackButton = styled.button`
+  position: absolute;
+  left: ${mainPadding}px;
+  padding: 0px;
+  text-align: center;
+  border: none;
+  background-color: none;
 `;
