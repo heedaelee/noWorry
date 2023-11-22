b77b8e7984b83a2bea02acfa7c50a801eb4ac687
heedaelee <adguy72@gmail.com>
2023-11-23 00:10:04 +0900

    refactoring: 상수에 page 추가

--- src/constants/atom-constant.ts
+++ src/constants/atom-constant.ts
@@ -1,7 +1,9 @@
 export const WORRY_LIST_KEY = 'worryList';
 export const BOTTOM_TAP_NAME = 'bottomTapName';
+export const PAGES = 'pages';
 
 export default {
   WORRY_LIST_KEY,
   BOTTOM_TAP_NAME,
+  PAGES,
 };
