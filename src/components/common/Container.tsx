9a26d533c9c8aa407d50251a48f651780460a3ee
heedaelee <adguy72@gmail.com>
2023-11-23 00:09:31 +0900

    refactoring : 바텀 네비 조건부 컴포넌트

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -3,28 +3,37 @@ import styled from 'styled-components';
 
 import {GlobalStyles, bottomTapWrapper, mainPadding} from 'styles/globalStyles';
 import BottomNavigation from 'components/bottomNavigation/BottomNavigation';
+import {useRecoilState} from 'recoil';
+import {pagesState} from 'store/pages';
+import Headers from 'components/headers/Headers';
+import {useChangePages} from 'hooks/useChagePages';
 
 interface ContainerProps {}
 
 const Container = ({}: ContainerProps) => {
+  const [pages, setPages] = useChangePages();
+
   return (
     <Wrapper>
+      <Headers />
       {/* 자식 라우터 시작 --> */}
       <Outlet />
       {/*  -->  끝 */}
-      <BottomNavigation />
+      {(pages === 'list' || pages === 'statics') && <BottomNavigation />}
     </Wrapper>
   );
 };
 
 // 가장 외부 Contanier
 const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
+  height: 100%;
+  /* display: flex;
+  flex-direction: column; */
   /* max-width로 가로 크기 최대값을 고정해준다 */
   max-width: 390px;
   margin: auto;
   padding: 0px;
+
   background-color: ${GlobalStyles.backgroundColor};
 `;
 
