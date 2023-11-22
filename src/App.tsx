d1ed7ff7331b11895626030c4d8793aa5b95928f
heedaelee <adguy72@gmail.com>
2023-11-23 00:08:30 +0900

    refactoring: 등록 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import {Route, Routes, useNavigate} from 'react-router-dom';
 import Container from 'components/common/Container';
 import Main from 'pages/Main';
 import Statics from 'pages/Statics';
+import Registor from 'pages/Registor';
 
 function App() {
   return (
     <Routes>
       <Route path='/' Component={Container}>
         <Route path='/' Component={Main} />
+        <Route path='/register' Component={Registor} />
         <Route path='/statics' Component={Statics} />
       </Route>
     </Routes>
