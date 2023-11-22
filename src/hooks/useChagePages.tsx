daecda9a4ed4ea2044918047b5b9bc01e8f254f7
heedaelee <adguy72@gmail.com>
2023-11-23 00:08:43 +0900

    add : change hook 추가

---
+++ src/hooks/useChagePages.tsx
@@ -0,0 +1,37 @@
+import {useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {SetterOrUpdater, useRecoilState} from 'recoil';
+import {PageType, pagesState} from 'store/pages';
+
+type useChagePagesType = () => [PageType, SetterOrUpdater<PageType>];
+/**
+ * @param
+ * @return [pages, setPages, changePages]
+ */
+export const useChangePages: useChagePagesType = () => {
+  const [pages, setPages] = useRecoilState(pagesState);
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    changePages(pages);
+  }, [pages]);
+
+  const changePages = (page: PageType) => {
+    switch (page) {
+      case 'list':
+        navigate('/');
+        break;
+      case 'editor':
+        navigate(`/${page}`);
+        break;
+      case 'register':
+        navigate(`/${page}`);
+        break;
+      default:
+        navigate('/');
+        break;
+    }
+  };
+
+  return [pages, setPages];
+};
