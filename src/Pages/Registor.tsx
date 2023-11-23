fb2b4a992f127ae92917dfd60c9a3a608f5e772e
heedaelee <adguy72@gmail.com>
2023-11-23 10:57:05 +0900

    refactoring: 등록 페이지 수정

--- src/Pages/Registor.tsx
+++ src/Pages/Registor.tsx
@@ -1,7 +1,25 @@
+import Form from 'components/worryForm/Form';
 import styled from 'styled-components';
+import {
+  HorizontalPaddingWrapper,
+  fullHeight,
+  headWrapper,
+  mainPadding,
+} from 'styles/globalStyles';
 
 const Registor = () => {
-  return <>등록페이지</>;
+  return (
+    <HorizontalPaddingWrapper>
+      <Wrapper>
+        <Form />
+      </Wrapper>
+    </HorizontalPaddingWrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  /* background-color: yellow; */
+  height: ${fullHeight - (headWrapper + mainPadding)}px;
+`;
 
 export default Registor;
