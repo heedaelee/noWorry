738e90f1f2fe275ede54d6159304bbb7caed80ef
heedaelee <adguy72@gmail.com>
2023-11-28 16:18:12 +0900

    feat :  모달 atom 상수 키 추가

--- src/constants/atom-constant.ts
+++ src/constants/atom-constant.ts
@@ -1,9 +1,13 @@
+import {v4 as uuidv4} from 'uuid';
+
 export const WORRY_LIST_KEY = 'worryList';
 export const BOTTOM_TAP_NAME = 'bottomTapName';
 export const PAGES = 'pages';
+export const MODAL_OPEN_KEY = `modalOpenAtom/${uuidv4()}`;
 
 export default {
   WORRY_LIST_KEY,
   BOTTOM_TAP_NAME,
   PAGES,
+  MODAL_OPEN_KEY,
 };
