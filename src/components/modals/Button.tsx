3f81a8f0596b6ece523602ba70da546b4fa9d2ad
heedaelee <adguy72@gmail.com>
2023-11-30 18:17:59 +0900

    feature : 모달/버튼 컴포넌트 추가
    타입 : confirm/cancel 형태로 분기

---
+++ src/components/modals/Button.tsx
@@ -0,0 +1,31 @@
+import styled, {CSSProperties} from 'styled-components';
+import {GlobalStyles} from 'styles/globalStyles';
+
+interface ButtonProps {
+  text: string;
+  type: 'confirm' | 'cancel';
+  style?: CSSProperties;
+  onClick: () => void;
+}
+
+const Button = ({text, style, type, onClick}: ButtonProps) => {
+  return (
+    <ButtonStyled $type={type} onClick={onClick} style={style}>
+      {text}
+    </ButtonStyled>
+  );
+};
+
+const ButtonStyled = styled.button<{$type: ButtonProps['type']}>`
+  width: 115px;
+  height: 46px;
+  background-color: ${({$type}) =>
+    $type === 'cancel' ? GlobalStyles.Colors.blue : 'white'};
+  color: ${({$type}) =>
+    $type === 'cancel' ? 'white' : GlobalStyles.Colors.blue};
+  font-family: ${GlobalStyles.fontFamilyType.regular};
+  border-radius: 16px;
+  border: 1px solid ${GlobalStyles.Colors.blue};
+`;
+
+export default Button;
