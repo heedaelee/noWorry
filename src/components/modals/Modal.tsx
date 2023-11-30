f39ba2da395d670eef8682811fd2ac66bde6a9cd
heedaelee <adguy72@gmail.com>
2023-11-30 18:17:26 +0900

    refactoring: 불필요한 wrapper 제거,
    모달 컴포넌트 내에서 모달 백그라운드를 가장 상단에서 호출하려했다가,
    불필요한 wrapper가 겹쳐지니 변수가 많아지니까, 그냥 모달 백그라운드에서 {children}으로 받아오는 모달 컴포넌트 랜더링하는 형식으로 호출하게 리팩토링 함

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -10,10 +10,7 @@ const Modal = ({isVisible, closeModal, children}: ModalType) => {
   return (
     <>
       {isVisible ? (
-        <Wrapper>
-          <ModalBackGround onClick={closeModal} />
-          <Container>{children}</Container>
-        </Wrapper>
+        <ModalBackGround onClick={closeModal}>{children}</ModalBackGround>
       ) : (
         <></>
       )}
@@ -21,24 +18,11 @@ const Modal = ({isVisible, closeModal, children}: ModalType) => {
   );
 };
 
-const Wrapper = styled.div`
-  width: 100vh;
-  height: 100vh;
-`;
 const ModalBackGround = styled.div`
   background-color: rgba(0, 0, 0, 0.6);
   width: 100%;
-  height: 100vh;
-  position: absolute;
-  bottom: 0;
-  right: 0;
-`;
-
-const Container = styled.div`
-  width: 100%;
   height: 100%;
-  position: absolute;
-  background-color: transparent;
+  position: fixed;
+  z-index: 1;
 `;
-
 export default Modal;
