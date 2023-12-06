b85653af9d28f172b745b1b905343e1e99de2099
heedaelee <adguy72@gmail.com>
2023-12-06 19:03:55 +0900

    feature : 스위치 버튼 초안 컴포넌트 구현

--- src/Pages/Statics.tsx
+++ src/Pages/Statics.tsx
@@ -1,3 +1,5 @@
+import Switch from 'components/switch/Switch';
+import Text from 'components/text/Text';
 import styled from 'styled-components';
 import {
   HeightBetweenHeaderAndContents,
@@ -7,7 +9,14 @@ import {
 const Statics = () => {
   return (
     <HorizontalPaddingWrapper>
-      <StaticsHeader></StaticsHeader>
+      <StaticsHeader>
+        <TitleWrapper>
+          <Text type='h15'>전체</Text>
+        </TitleWrapper>
+        <SwitchWrapper>
+          <Switch menuText={['전체', '월별']} />
+        </SwitchWrapper>
+      </StaticsHeader>
     </HorizontalPaddingWrapper>
   );
 };
@@ -16,8 +25,25 @@ const StaticsHeader = styled.div`
   display: flex;
   flex-direction: row;
   height: ${HeightBetweenHeaderAndContents}px;
+  justify-content: center;
   align-items: center;
-  /* border: 1px solid black; */
+  position: relative;
+  margin: 25px 0px;
+  border: 1px solid black;
+`;
+const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+const SwitchWrapper = styled.div`
+  position: absolute;
+  top: 21px;
+  right: 16px;
+  width: 107px;
+  height: 38px;
+  border: 1px solid black;
 `;
 
 export default Statics;
