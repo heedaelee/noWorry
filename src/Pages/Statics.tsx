cda258ef72a890d7221c4f81afc424a7c2bdec8d
heedaelee <adguy72@gmail.com>
2023-12-07 17:29:03 +0900

    feature : 통계 초안 view 작성

--- src/Pages/Statics.tsx
+++ src/Pages/Statics.tsx
@@ -2,7 +2,10 @@ import Switch from 'components/switch/Switch';
 import Text from 'components/text/Text';
 import {useState} from 'react';
 import styled from 'styled-components';
-import {HorizontalPaddingWrapper} from 'styles/globalStyles';
+import {
+  HorizontalPaddingWrapper,
+  staticsContentsHeight,
+} from 'styles/globalStyles';
 
 const Statics = () => {
   const [isMonth, setIsMonth] = useState(false);
@@ -20,18 +23,24 @@ const Statics = () => {
           />
         </SwitchWrapper>
       </StaticsHeader>
-      <StaticsContents></StaticsContents>
+      <StaticsContents>
+        <ContentsWrapper>
+          <Text type='sub1'>걱정이 일어나지 않을 확률</Text>
+          <GraphWrapper></GraphWrapper>
+          <Text
+            style={{
+              whiteSpace: 'pre-wrap',
+            }}
+            type='sub1'>{`통계를 낼 수 있는\n기록이 없어요!`}</Text>
+        </ContentsWrapper>
+      </StaticsContents>
     </HorizontalPaddingWrapper>
   );
 };
 
 const StaticsHeader = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
   position: relative;
-  margin: 15px 0px;
+  margin: 15px 0px 35px 0px;
 `;
 const TitleWrapper = styled.div`
   display: flex;
@@ -50,10 +59,23 @@ const SwitchWrapper = styled.div`
   justify-content: center;
 `;
 const StaticsContents = styled.div`
-  width: 350px;
+  width: 358px;
+  height: ${staticsContentsHeight}px;
   display: flex;
   flex-direction: column;
-  justify-content: center;
 `;
+const ContentsWrapper = styled.div`
+  height: 262px;
+  width: 358px;
+  background-color: white;
+  border-radius: 12px;
+  box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.08);
+  display: flex;
+  padding: 40px var(--space-0, 0px);
+  flex-direction: column;
+  align-items: center;
+  gap: 60px;
+`;
+const GraphWrapper = styled.div``;
 
 export default Statics;
