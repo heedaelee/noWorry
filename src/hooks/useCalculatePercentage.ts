c80b2088d556a17ba7d305ac4c35ac6b1f85328a
heedaelee <adguy72@gmail.com>
2023-12-09 20:16:31 +0900

    refactoring : 필터링에서 현재걱정 상태는 제외, 변수명 변경

--- src/hooks/useCalculatePercentage.ts
+++ src/hooks/useCalculatePercentage.ts
@@ -3,32 +3,34 @@ import {useMemo} from 'react';
 import {WorryList} from 'types/common';
 
 const useCalculatePercentage = (worryList: WorryList) => {
-  const totalItemsCount = worryList.length;
+  const totalItemsCount = worryList.filter(
+    itme => itme.worryStatus !== '현재 걱정',
+  ).length;
   const notHappenedItemCount = useMemo(() => {
     return worryList.filter(item => item.worryStatus === '일어나지 않음')
       .length;
   }, [worryList]);
 
   const makeData = () => {
-    const percentage = (notHappenedItemCount / totalItemsCount) * 100;
-
+    const notHappenedRate = (notHappenedItemCount / totalItemsCount) * 100;
     /* 데이터set 만들기 */
     const data: PieChartProps['data'] = [
       //  일어남
       {
         id: '0',
-        value: percentage,
+        value: notHappenedRate,
       },
       //  일어나지 않음
       {
         id: '1',
-        value: 100 - percentage,
+        value: 100 - notHappenedRate,
       },
     ];
 
-    return {data};
+    return data;
   };
-  return makeData();
+  const data = makeData();
+  return {data, totalItemsCount, notHappenedItemCount};
 };
 
 export default useCalculatePercentage;
