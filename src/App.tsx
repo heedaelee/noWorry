415097831500e2c8f05dc0cacf7f4f0494722c6b
heedaelee <adguy72@gmail.com>
2023-12-12 23:18:05 +0900

    feature : 라우터 셋팅 페이지 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Container from 'components/common/Container';
 import Main from 'pages/Main';
 import Statics from 'pages/Statics';
 import Registor from 'pages/Registor';
+import Setting from 'pages/Setting';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' Component={Main} />
         <Route path='/register' Component={Registor} />
         <Route path='/statics' Component={Statics} />
+        <Route path='/setting' Component={Setting} />
       </Route>
     </Routes>
   );
