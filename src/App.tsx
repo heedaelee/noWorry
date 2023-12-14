56d1b1731f962597305b35ae876f666b63178daf
heedaelee <adguy72@gmail.com>
2023-12-14 11:46:22 +0900

    refactoring : 라우터 코드스플리팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
+import {Suspense, lazy} from 'react';
 import {Route, Routes} from 'react-router-dom';
 
-import Container from 'components/common/Container';
-import Main from 'pages/Main';
-import Statics from 'pages/Statics';
-import Registor from 'pages/Registor';
-import Setting from 'pages/Setting';
+// import Container from 'components/common/Container';
+// import Main from 'pages/Main';
+// import Statics from 'pages/Statics';
+// import Registor from 'pages/Registor';
+// import Setting from 'pages/Setting';
+
+// Route-based 코드스플리팅
+
+const Container = lazy(() => import('components/common/Container'));
+
+const Main = lazy(() => import('pages/Main'));
+const Registor = lazy(() => import('pages/Registor'));
+const Statics = lazy(() => import('pages/Statics'));
+const Setting = lazy(() => import('pages/Setting'));
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' Component={Container}>
-        <Route path='/' Component={Main} />
-        <Route path='/register' Component={Registor} />
-        <Route path='/statics' Component={Statics} />
-        <Route path='/setting' Component={Setting} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinnner />}>
+      <Routes>
+        <Route path='/' Component={Container}>
+          <Route path='/' Component={Main} />
+          <Route path='/register' Component={Registor} />
+          <Route path='/statics' Component={Statics} />
+          <Route path='/setting' Component={Setting} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
